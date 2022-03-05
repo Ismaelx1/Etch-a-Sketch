c5f2a36d15022c44cc738ce954b91f97789afd8d
Ismail Askhatir <spxoki@gmail.com>
2022-03-05 17:25:22 +0100

    only event listener left

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const colorBtn = document.querySelector('#color-btn')
 
 
 
-makeBoth(2, 2)
+
+makeBoth(4, 4)
 
 function makeBoth(rowNum, cellNum) {
     for (r = 0; r < rowNum; r++) {
@@ -19,8 +20,10 @@ function makeBoth(rowNum, cellNum) {
             for (j = 0; j < cellNum; j++) {
                 let newCell = document.createElement("div");
                 rows[j].appendChild(newCell).className = "cell";
+                    // let cellId = document.querySelectorAll('')
             };
         };
+        container.style.backgroundColor = 'white'
     };
 
     
@@ -49,9 +52,16 @@ container.style.backgroundColor = 'white'
 
 function black() {
 
-container.addEventListener('click', colors, false)
+container.addEventListener('mouseover', colors, false)
 function colors(event) {
-  event.target.style.backgroundColor = 'black'
+  
+  if (event.target === thingy || event.target === container) {
+    false
+  
+} else {
+    event.target.style.backgroundColor = 'black'
+}
+
 
 }
 
@@ -60,33 +70,45 @@ function colors(event) {
 
 
 function multi() {
-    container.addEventListener('click', colors, false)
+    container.addEventListener('mouseover', colors, false)
     function colors(event) {
         let random1 = Math.floor(Math.random() * 256)
         let random2 = Math.floor(Math.random() * 256)
         let random3 = Math.floor(Math.random() * 256)
 
-      event.target.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`
+      
 
-      if (event.target === firstthing) {
-          console.log("Right")
+
+      if (event.target === thingy || event.target === container) {
+        false
+      
+    } else {
+        event.target.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`
+    }
+  
       }
    
      } 
-    }
+    
     
     function empty() {
 
-        container.addEventListener('click', colors, false)
+        container.addEventListener('mouseover', colors, false)
         function colors(event) {
-          event.target.style.backgroundColor = 'white'
-          console.log(event.target)
-        }
-        
-        
-        }
-        
+         
+         
+            if (event.target === thingy || event.target === container) {
+                false
+              
+            } else {
+              event.target.style.backgroundColor = 'white'
+            }
+          }
 
-        let firstthing = document.querySelector("#container > div:nth-child(1) > div:nth-child(1)")
-        
+        }
+        let thingy = document.querySelectorAll("#container > div")
+
+  
+  
+        console.log(thingy)
     
